1fea8135b84ac63cb93bd267a3b0dbfa50ba5c59
Allan77-debug <allan.paredes@correounivalle.edu.co>
2024-10-13 23:00:40 -0500

    Borrado de metodo basura

--- src/main/java/com/example/sudokufpoe/Controller/SudokuController.java
+++ src/main/java/com/example/sudokufpoe/Controller/SudokuController.java
@@ -154,19 +154,6 @@ public class SudokuController {
     }
 
     /**
-     * Handles invalid input.
-     *
-     * @param key the invalid input string
-     */
-    private void handleInvalidInput(String key) {
-        if (key.isEmpty() || !key.matches("[1-6]")) {
-            System.out.println("Invalid input. Only numbers from 1 to 6 are allowed.");
-        } else {
-            System.out.println("Number out of range. Enter a number between 1 and 6.");
-        }
-    }
-
-    /**
      * Clears the content of a cell.
      *
      * @param row the row of the cell
